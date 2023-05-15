489725735f5331171714d90a099f0d60632adc8f
xpan-eileen <xpan.eileen@gmail.com>
2023-05-15 14:27:44 +0100

    Update SmallGrps.gd

--- gap/SmallGrps.gd
+++ gap/SmallGrps.gd
@@ -9,10 +9,11 @@
 ##
 ##
 ##
-#! @Description
-#!  Compute the list of isomorphism classes of groups of order $n$.
 #!
 DeclareGlobalFunction("AllSOTGroups");
+#! @Description
+#!  computes the list of isomorphism classes of groups of order $n$.
+
 #! @BeginExampleSession
 #! gap> AllSOTGroups(3*5*7*11);
 #! [ <pc group of size 1155 with 4 generators>, <pc group of size 1155 with 4 generators>,
@@ -21,15 +22,22 @@ DeclareGlobalFunction("AllSOTGroups");
 
 #!
 DeclareGlobalFunction("SOTGroup");
-
+#! @Description
+#!  returns the $i$-th group of order $n$ in the list.
 #!
 DeclareGlobalFunction("NumberOfSOTGroups");
-
-#!
-DeclareGlobalFunction("SOTGroupIsAvailable");
-
-#!
-DeclareGlobalFunction("SOTGroupsInformation");
+#! Description
+#!  returns the number of isomorphism types of groups of order $n$.
 
 #!
 DeclareGlobalFunction("IdSOTGroup");
+#! @Description
+#!  determines the SOT library number of $G$; that is, the function returns a pair [order, i] where $G$ is isomorphic to SOTGroup( order, i ).
+
+#!
+DeclareGlobalFunction("SOTGroupIsAvailable");
+#! @Description
+#!  returns true if the order is covered in the SOTGrps package.
+
+#!
+DeclareGlobalFunction("SOTGroupsInformation");
