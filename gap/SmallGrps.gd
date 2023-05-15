ff2727973108bf09fc6262c85370521ce7d5ff0c
xpan-eileen <xpan.eileen@gmail.com>
2023-05-16 00:17:45 +0100

    Update SmallGrps.gd

--- gap/SmallGrps.gd
+++ gap/SmallGrps.gd
@@ -1,11 +1,10 @@
 #############################################################################
-#! @Chapter The SOTGrps Library
-#! With some overlaps, the SOTGrps library extends the Small Group Library to give access to some more
-#!    "small" orders. For example, in addition to the existing Small Groups Library, it constructs a
+#! @Chapter The SOTGrps package
+#! With some overlaps, the SOTGrps package extends the Small Group Library to give access to some more
+#!    "small" orders. For example, it constructs a
 #!    complete and irredundant list of isomorphism type representatives of the groups of order
 #!    - that factorises into at most four primes;
-#!    - p^4q.
-##
+#!    - p^4q, for any primes p and q.
 ##
 
 ###############################
@@ -13,26 +12,28 @@
 ##
 ##
 #! @Section Main functions
-#!
-DeclareGlobalFunction("AllSOTGroups");
 #! @Description
-#!  computes the list of isomorphism classes of groups of order $n$.
-
+#!  computes the list of isomorphism classes of groups of order <A>n</A>.
+#! Solvable groups are given as PcGroup, unless USE_PCP is turned on, in which case the groups are constructed as PcpGroup.
+#! Nonsolvable groups are given 
+#! @Arguments n
 #! @BeginExampleSession
 #! gap> AllSOTGroups(3*5*7*11);
 #! [ <pc group of size 1155 with 4 generators>, <pc group of size 1155 with 4 generators>,
 #!  <pc group of size 1155 with 4 generators>, <pc group of size 1155 with 4 generators> ]
 #! @EndExampleSession
+DeclareGlobalFunction("AllSOTGroups");
 
-#!
-DeclareGlobalFunction("SOTGroup");
 #! @Description
 #!  returns the <A>i</A>-th group of order <A>n</A> in the list.
 #! @Arguments n, i
-DeclareGlobalFunction("NumberOfSOTGroups");
-#! Description
-#!  returns the number of isomorphism types of groups of order $n$.
+DeclareGlobalFunction("SOTGroup");
 
+#! Description
+#!  returns the number of isomorphism types of groups of order <A>n</A>.
+#! @Arguments n
+#
+DeclareGlobalFunction("NumberOfSOTGroups");
 #!
 DeclareGlobalFunction("IdSOTGroup");
 #! @Description
