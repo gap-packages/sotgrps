fc3361e3873eb207d28a0e5bf7ab85b43a0df61b
xpan-eileen <xpan.eileen@gmail.com>
2023-05-25 15:28:41 +0100

    Update error message in manual

--- gap/SOTGrps.gd
+++ gap/SOTGrps.gd
@@ -38,7 +38,7 @@ DeclareGlobalFunction("AllSOTGroups");
 #! gap> NumberOfSOTGroups(2*3*5*7);
 #! 12
 #! gap> NumberOfSOTGroups(2*3*5*7*11);
-#! Error, Order 2310 is not available
+#! Error, Order 2310 is not supported by SOTGrps, please refer to the documentation of function NumberOfSOTGroups for the list of suppoorted orders.
 #! @EndExampleSession
 DeclareGlobalFunction("NumberOfSOTGroups");
 
--- tst/sotgrps01.tst
+++ tst/sotgrps01.tst
@@ -21,7 +21,7 @@ gap> AllSOTGroups(60);
 gap> NumberOfSOTGroups(2*3*5*7);
 12
 gap> NumberOfSOTGroups(2*3*5*7*11);
-Error, Order 2310 is not available
+Error, Order 2310 is not supported by SOTGrps, please refer to the documentation of function NumberOfSOTGroups for the list of suppoorted orders.
 
 # doc/_Chapter_The_SOTGrps_package.xml:66-69
 gap> SOTGroup(2*3*5*7, 1);
